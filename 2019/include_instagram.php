e5885cc026a9b5c49f0592feffbb83aff9448710
fesecalo <fesecalo@gmail.com>
2023-05-03 19:51:37 -0400

    Update include_instagram.php

--- 2019/include_instagram.php
+++ 2019/include_instagram.php
@@ -3,11 +3,7 @@
             <div class="container">
             <h2 class="title"><img src="img/inst3.png" style="max-width:40%" alt="@tlccourier" /></h2>
             <div>
-            	<!-- LightWidget WIDGET -->
-            	<script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-            	<iframe src="//lightwidget.com/widgets/30618b5bd40d57a8a935895002efb972.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;">
-            		
-            	</iframe>
+            	<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/41cca02b8cbf5078809e383f7dfc3472.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
             </div>
                 
             </div>
