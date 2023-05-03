608138843493ecc0a38cd7a7c8276d8e879b3632
fesecalo <fesecalo@gmail.com>
2023-05-03 19:18:07 -0400

    Se actualiza widget

--- 2019/include_instagram.php
+++ 2019/include_instagram.php
@@ -1,11 +1,16 @@
-   <!-- BLOCK "TYPE 7" -->
-            <div class="block type-7 scroll-to-block" data-id="contact">
-                <div class="container">
-                <h2 class="title"><img src="img/inst3.png" style="max-width:40%" alt="@tlccourier" /></h2>
-                <div><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/204c46ba401651b4aa3d01f6a7e20da2.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
-                         </div>
-                    
-                </div>
+<!-- BLOCK "TYPE 7" -->
+        <div class="block type-7 scroll-to-block" data-id="contact">
+            <div class="container">
+            <h2 class="title"><img src="img/inst3.png" style="max-width:40%" alt="@tlccourier" /></h2>
+            <div>
+            	<!-- LightWidget WIDGET -->
+            	<script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+            	<iframe src="//lightwidget.com/widgets/30618b5bd40d57a8a935895002efb972.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;">
+            		
+            	</iframe>
             </div>
-            <!-- BLOCK "TYPE 7" -->
+                
+            </div>
+        </div>
+<!-- BLOCK "TYPE 7" -->
             
